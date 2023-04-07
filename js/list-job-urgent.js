41319ea77677cbde68aad54e71e25e931ef7c08a
NguyenBangPhi <bangphin1@gmail.com>
2023-04-07 13:36:37 +0700

    Upload Source

---
+++ js/list-job-urgent.js
@@ -0,0 +1,212 @@
+const _$ = document.querySelector.bind(document);
+const loadDataList = () => {
+    let wrapData = _$('#loadDataList')
+    let html = ``
+
+    html += /*html*/`<div class="mng-company d-flex">
+    <div class="col-md-1 col-sm-2 m-d-none">
+        <div class="mng-company-pic"><a href="/viec-lam/156027-nhan-vien-ke-toan" data-pjax="false"
+                rel="nofollow"><img
+                    src="./img/citci.jpg"
+                    class="img-responsive lazy-normal" alt="CÔNG TY CP 28 QUẢNG NGÃI"
+                    title=" Nhân Viên Kế Toán"></a></div>
+    </div>
+
+    <div class="col-md-8 col-sm-5 m-pd-0 ">
+        <div class="job-info">
+            <p class="job-title">
+                <a href="/viec-lam/156027-nhan-vien-ke-toan" class="" title=" Nhân Viên Kế Toán">
+                    <span class="i-title"> Nhân Viên Kế Toán</span> <span class="label new">NEW</span> </a>
+            </p>
+
+            <p class="name d-text-overflow">
+                <a class="company-title" href="/gian-tuyen-dung/cong-ty-cp-28-quang-ngai-31827"
+                    title="CÔNG TY CP 28 QUẢNG NGÃI">CÔNG TY CP 28 QUẢNG NGÃI</a>
+            </p>
+            <p class="location">
+                <span><i class="fa fa-map-marker"></i>
+                    <a target="_blank" href="/tim-viec/quang-ngai" title="Quảng Ngãi">Quảng Ngãi</a>
+                </span>
+                <span><i class="fa fa-dollar"></i> Thỏa thuận</span>
+                <span><i class="fa fa-user"></i> 1 người</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-4 text-right">
+        <div class="job-date">
+            <p class="expired">Hạn cuối: <strong>04/05/2023</strong></p>
+            <p class="detail">
+                <a rel="nofollow" class="btn btn-apply-s m-width-100"
+                    href="/viec-lam/156027-nhan-vien-ke-toan#form-apply"><i class="fa fa-paper-plane"></i> Ứng tuyển
+                    ngay</a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="mng-company d-flex">
+    <div class="col-md-1 col-sm-2 m-d-none">
+        <div class="mng-company-pic"><a href="/viec-lam/156027-nhan-vien-ke-toan" data-pjax="false"
+                rel="nofollow"><img
+                    src="./img/citci.jpg"
+                    class="img-responsive lazy-normal" alt="CÔNG TY CP 28 QUẢNG NGÃI"
+                    title=" Nhân Viên Kế Toán"></a></div>
+    </div>
+
+    <div class="col-md-8 col-sm-5 m-pd-0 ">
+        <div class="job-info">
+            <p class="job-title">
+                <a href="/viec-lam/156027-nhan-vien-ke-toan" class="" title=" Nhân Viên Kế Toán">
+                    <span class="i-title"> Nhân Viên Kế Toán</span> <span class="label new">NEW</span> </a>
+            </p>
+
+            <p class="name d-text-overflow">
+                <a class="company-title" href="/gian-tuyen-dung/cong-ty-cp-28-quang-ngai-31827"
+                    title="CÔNG TY CP 28 QUẢNG NGÃI">CÔNG TY CP 28 QUẢNG NGÃI</a>
+            </p>
+            <p class="location">
+                <span><i class="fa fa-map-marker"></i>
+                    <a target="_blank" href="/tim-viec/quang-ngai" title="Quảng Ngãi">Quảng Ngãi</a>
+                </span>
+                <span><i class="fa fa-dollar"></i> Thỏa thuận</span>
+                <span><i class="fa fa-user"></i> 1 người</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-4 text-right">
+        <div class="job-date">
+            <p class="expired">Hạn cuối: <strong>04/05/2023</strong></p>
+            <p class="detail">
+                <a rel="nofollow" class="btn btn-apply-s m-width-100"
+                    href="/viec-lam/156027-nhan-vien-ke-toan#form-apply"><i class="fa fa-paper-plane"></i> Ứng tuyển
+                    ngay</a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="mng-company d-flex">
+    <div class="col-md-1 col-sm-2 m-d-none">
+        <div class="mng-company-pic"><a href="/viec-lam/156027-nhan-vien-ke-toan" data-pjax="false"
+                rel="nofollow"><img
+                    src="./img/citci.jpg"
+                    class="img-responsive lazy-normal" alt="CÔNG TY CP 28 QUẢNG NGÃI"
+                    title=" Nhân Viên Kế Toán"></a></div>
+    </div>
+
+    <div class="col-md-8 col-sm-5 m-pd-0 ">
+        <div class="job-info">
+            <p class="job-title">
+                <a href="/viec-lam/156027-nhan-vien-ke-toan" class="" title=" Nhân Viên Kế Toán">
+                    <span class="i-title"> Nhân Viên Kế Toán</span> <span class="label new">NEW</span> </a>
+            </p>
+
+            <p class="name d-text-overflow">
+                <a class="company-title" href="/gian-tuyen-dung/cong-ty-cp-28-quang-ngai-31827"
+                    title="CÔNG TY CP 28 QUẢNG NGÃI">CÔNG TY CP 28 QUẢNG NGÃI</a>
+            </p>
+            <p class="location">
+                <span><i class="fa fa-map-marker"></i>
+                    <a target="_blank" href="/tim-viec/quang-ngai" title="Quảng Ngãi">Quảng Ngãi</a>
+                </span>
+                <span><i class="fa fa-dollar"></i> Thỏa thuận</span>
+                <span><i class="fa fa-user"></i> 1 người</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-4 text-right">
+        <div class="job-date">
+            <p class="expired">Hạn cuối: <strong>04/05/2023</strong></p>
+            <p class="detail">
+                <a rel="nofollow" class="btn btn-apply-s m-width-100"
+                    href="/viec-lam/156027-nhan-vien-ke-toan#form-apply"><i class="fa fa-paper-plane"></i> Ứng tuyển
+                    ngay</a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="mng-company d-flex">
+    <div class="col-md-1 col-sm-2 m-d-none">
+        <div class="mng-company-pic"><a href="/viec-lam/156027-nhan-vien-ke-toan" data-pjax="false"
+                rel="nofollow"><img
+                    src="./img/citci.jpg"
+                    class="img-responsive lazy-normal" alt="CÔNG TY CP 28 QUẢNG NGÃI"
+                    title=" Nhân Viên Kế Toán"></a></div>
+    </div>
+
+    <div class="col-md-8 col-sm-5 m-pd-0 ">
+        <div class="job-info">
+            <p class="job-title">
+                <a href="/viec-lam/156027-nhan-vien-ke-toan" class="" title=" Nhân Viên Kế Toán">
+                    <span class="i-title"> Nhân Viên Kế Toán</span> <span class="label new">NEW</span> </a>
+            </p>
+
+            <p class="name d-text-overflow">
+                <a class="company-title" href="/gian-tuyen-dung/cong-ty-cp-28-quang-ngai-31827"
+                    title="CÔNG TY CP 28 QUẢNG NGÃI">CÔNG TY CP 28 QUẢNG NGÃI</a>
+            </p>
+            <p class="location">
+                <span><i class="fa fa-map-marker"></i>
+                    <a target="_blank" href="/tim-viec/quang-ngai" title="Quảng Ngãi">Quảng Ngãi</a>
+                </span>
+                <span><i class="fa fa-dollar"></i> Thỏa thuận</span>
+                <span><i class="fa fa-user"></i> 1 người</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-4 text-right">
+        <div class="job-date">
+            <p class="expired">Hạn cuối: <strong>04/05/2023</strong></p>
+            <p class="detail">
+                <a rel="nofollow" class="btn btn-apply-s m-width-100"
+                    href="/viec-lam/156027-nhan-vien-ke-toan#form-apply"><i class="fa fa-paper-plane"></i> Ứng tuyển
+                    ngay</a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="mng-company d-flex">
+    <div class="col-md-1 col-sm-2 m-d-none">
+        <div class="mng-company-pic"><a href="/viec-lam/156027-nhan-vien-ke-toan" data-pjax="false"
+                rel="nofollow"><img
+                    src="./img/citci.jpg"
+                    class="img-responsive lazy-normal" alt="CÔNG TY CP 28 QUẢNG NGÃI"
+                    title=" Nhân Viên Kế Toán"></a></div>
+    </div>
+
+    <div class="col-md-8 col-sm-5 m-pd-0 ">
+        <div class="job-info">
+            <p class="job-title">
+                <a href="/viec-lam/156027-nhan-vien-ke-toan" class="" title=" Nhân Viên Kế Toán">
+                    <span class="i-title"> Nhân Viên Kế Toán</span> <span class="label new">NEW</span> </a>
+            </p>
+
+            <p class="name d-text-overflow">
+                <a class="company-title" href="/gian-tuyen-dung/cong-ty-cp-28-quang-ngai-31827"
+                    title="CÔNG TY CP 28 QUẢNG NGÃI">CÔNG TY CP 28 QUẢNG NGÃI</a>
+            </p>
+            <p class="location">
+                <span><i class="fa fa-map-marker"></i>
+                    <a target="_blank" href="/tim-viec/quang-ngai" title="Quảng Ngãi">Quảng Ngãi</a>
+                </span>
+                <span><i class="fa fa-dollar"></i> Thỏa thuận</span>
+                <span><i class="fa fa-user"></i> 1 người</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-4 text-right">
+        <div class="job-date">
+            <p class="expired">Hạn cuối: <strong>04/05/2023</strong></p>
+            <p class="detail">
+                <a rel="nofollow" class="btn btn-apply-s m-width-100"
+                    href="/viec-lam/156027-nhan-vien-ke-toan#form-apply"><i class="fa fa-paper-plane"></i> Ứng tuyển
+                    ngay</a>
+            </p>
+        </div>
+    </div>
+</div>`
+    wrapData.innerHTML = html;
+}
